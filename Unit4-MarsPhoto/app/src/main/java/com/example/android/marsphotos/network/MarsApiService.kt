73efaa3b3cc9c74dba5e2c283931fbbce127aea4
vietcong00 <57753943+vietcong00@users.noreply.github.com>
2022-03-02 00:47:53 +0700

    push final code Unit4-MarsPhoto

---
+++ Unit4-MarsPhoto/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,26 @@
+package com.example.android.marsphotos.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+//Add the following constant for the base URL for the web service.
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+
+val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    @GET("photos")
+    suspend fun getPhotos(): List<MarsPhoto>
+}
